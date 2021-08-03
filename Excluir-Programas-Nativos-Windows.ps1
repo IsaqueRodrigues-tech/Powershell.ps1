a14d5c9b0babc61288dd224d731f61400dae80b7
Isaque Rodrigues <62602321+IsaqueRodrigues-tech@users.noreply.github.com>
2021-08-03 12:26:07 -0300

    Create Excluir-Programas-Nativos-Windows.ps1

---
+++ Excluir-Programas-Nativos-Windows.ps1
@@ -0,0 +1,22 @@
+3D Builder: Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+Alarmes e Relógio: Get-AppxPackage *windowsalarms* | Remove-AppxPackage
+Calculadora: Get-AppxPackage *windowscalculator* | Remove-AppxPackage
+Calendário e o Email: *windowscommunicationsapps* | Remove-AppxPackage
+Câmera: *windowscamera* | Remove-AppxPackage
+Tenha o Office: Get-AppxPackage *officehub* | Remove-AppxPackage
+Tenha o Skype: Get-AppxPackage *skypeapp* | Remove-AppxPackage
+Groove Music: Get-AppxPackage *zunemusic* | Remove-AppxPackage
+Mapas: Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+Solitaire Collection: Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+MSN Dinheiro: Get-AppxPackage *bingfinance* | Remove-AppxPackage
+o Filmes e TV: Get-AppxPackage *zunevideo* | Remove-AppxPackage
+Notícias: Get-AppxPackage *bingnews* | Remove-AppxPackage
+OneNote: Get-AppxPackage *onenote* | Remove-AppxPackage
+Pessoas: Get-AppxPackage *people* | Remove-AppxPackage
+Phone Companion: Get-AppxPackage *windowsphone* | Remove-AppxPackage
+Fotos: Get-AppxPackage *photos* | Remove-AppxPackage
+Loja: Get-AppxPackage *windowsstore* | Remove-AppxPackage
+Esportes: Get-AppxPackage *bingsports* | Remove-AppxPackage
+Gravador de Voz: Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+Clima: Get-AppxPackage *bingweather* | Remove-AppxPackage
+Xbox: Get-AppxPackage *xboxapp* | Remove-AppxPackage
